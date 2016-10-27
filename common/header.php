6c88eed9ed5906a6a4573b87a855fc7e099b1879
Robert Bacon <mrrobacon@gmail.com>
2016-10-27 17:29:41 +0100

    Intial header defined, included within index.php

---
+++ common/header.php
@@ -0,0 +1,5 @@
+<head>
+    <meta charset="utf-8" />
+    
+    <link type="text/css" rel="stylesheet" href="/com1025_cw1/css/group.css">    
+</head>
--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html lang="en">
-<head>
-<title> </title>
-<meta http-equiv="Content-type" content="text/html;charset=UTF-8">
-</head>
-<body>
+    <?php include('/common/header.php') ?>
 
-</body>
+    <head>
+        <title> </title>
+        <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+    </head>
+    <body>
+        Hello World
+    </body>
 </html>
