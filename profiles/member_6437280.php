8d6defa3d9fb5f8a980ea5b20d1109e36a90ad70
Robbie_Murray <robbodym@gmail.com>
2016-11-01 12:59:15 +0000

    Added profile file for myself, Indented Sams profile, changed my name on the index file

---
+++ profiles/member_6437280.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en">
+    <?php include('../common/header.php') ?>
+
+    <head>
+        <title> Robbie's Awesome Page duh </title>
+        <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+    </head>
+    <body>
+    </body>
+</html>
