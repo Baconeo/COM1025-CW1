d26c84bfe51ca25e0477759b8b329409676b9914
Sam Rowe <samrowe98@gmail.com>
2016-11-01 13:10:19 +0000

    Updated Sam page

--- profiles/member_6407279.php
+++ profiles/member_6407279.php
@@ -1,29 +1,56 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html lang="en">
-    <head>
-        <title>Sam's Personal Page</title>
-        <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
-        <style type="text/css">
-        h1.main{text-align:center; font-weight:bold;}
-        div{background-color:#C5EDAD}
-        a{font-weight:bold; text-decoration:none; color:black; position:relative; right:110px}
-        a:hover{text-decoration:underline;}
-        </style>
-    </head>
-    <body>
-        <div>
-            <h1 class="main"> Sam's Personal Page </h1>
-            <br> <hr>
-        </div>
-        <h4 style="text-align:right;"> Navigate back to Homepage:<br><a href="../index.php">- Home page</a></h4>
-        <br>
-        <h3>General Information</h3>
-        <p>This page contains information about my life. It has details about my hobbies, interests and about my life in general. I am currently a student, in my first year, at the University of Surrey and I am studying Computing and Information Technology. My hobbies and interests are very varied as I enjoy many different things. Some of my hobbies and interests include:</p>
-        <ul>
-            <li> Watching and playing rugby</li>
-            <li> Anything to do with Harry Potter </li>
-            <li> Socialising with friends </li>
-            <li> Listening to music. (Especially Rock and Blues)</li>
-        </ul>
-    </body>
+<?php include('../common/header.php') ?>
+<head>
+<title>Sam's Personal Page</title>
+<meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+<style type="text/css">
+h1.main{text-align:center; font-weight:bold;}
+div{background-color:#C5EDAD;}
+a{font-weight:bold; text-decoration:none; color:black; position:relative; right:110px;}
+a:hover{text-decoration:underline;}
+h3{text-decoration:underline;}
+#serious{font-weight:bold;color:red;text-decoration:underline;}
+</style>
+</head>
+<body>
+<div>
+<h1 class="main"> Sam's Personal Page </h1>
+<hr>
+</div>
+<h4 style="text-align:right;"> Navigate back to Home page:<br><a href="../index.php">- Home page</a></h4>
+<br>
+<h3>General Information</h3>
+<p>This page contains information about my life currently. It has details about my hobbies, interests and about my life in general. Previously, I went to Christ's College Sixth form in Guildford in which I got my A levels and made some very good friends. I am currently a student, in my first year, at the University of Surrey and I am studying Computing and Information Technology. I find my studies challenging but I take them <span id="serious">very seriously</span> as I want to have a successful career in my chosen field. I also have many hobbies and interests that are very varied as I enjoy many different things. Some of my hobbies and interests include:</p>
+<ul>
+<li> Watching and playing rugby</li>
+<li> Anything to do with Harry Potter </li>
+<li> Socialising with friends </li>
+<li> Spending time with family </li>
+<li> Listening to music. (Especially Rock and Blues)</li>
+<li> Playing Video Games </li>
+</ul>
+<h3>Information about my interests</h3>
+<p>Although these are my main interests, I do enjoy doing things outside of the activities listed above such as walking my dog and cooking. However, I find that the hobbies and interests that I have listed above to be the things which I enjoy the most and have the best time while I am doing them. I have had many different influences which have led me to have to have such a diverse range of interests. These range from other family members interests to enjoyment of American culture. Below, I have again listed out my different hobbies and where I first became interested with them.</p>
+<table border="1" cellspacing="2" cellpadding="2">
+<caption>A table listing my interests and their influences</caption>
+<thead><tr>
+<th>Interest</th>
+<th>Influence</th>
+</tr></thead>
+<tr>
+<td>Rugby</td>
+<td>My Dad played rugby so I started playing rugby</td>
+</tr>
+<tr>
+<td rowspan="2">Harry Potter</td>
+<td>I watched the films and really enjoyed them</td>
+<td>I read the books and loved them</td>
+</tr>
+
+
+
+
+</table>
+</body>
 </html>
