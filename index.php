5c3b0647e56de598b0be496feb2db537736e8ae7
RobbieMurray <robbodym@gmail.com>
2016-11-03 11:31:10 +0000

    Added the text over image for everyperson, added a profile file for Thush

--- index.php
+++ index.php
@@ -17,17 +17,42 @@
         <h1 style="text-align:center">Group 15's Page!</h1>
 		<p>This is group 15's main page! The members of our group are:</p>
 		<ul>
-                    <li>Callum Seymour <a href="profiles/member_6426813.php" target=_blank><img src = "images/Placeholder_person.jpg" class = "pics"></a></li>
-                    <li>Sam Rowe <a href="profiles/member_6407279.php" target="_blank"><img src = "images/Placeholder_person.jpg" class="pics"></a></li>
+                    <li>
+                        <a href="profiles/member_6426813.php" target=_blank>
+                            <img src = "images/Placeholder_person.jpg" class="pics">
+                            <div class="textoverlay"><p class="names">Callum Seymour </p></div>
+                        </a>
+                    </li>
+                    <li> 
+                        <a href="profiles/member_6407279.php" target="_blank">
+                            <img src = "images/Placeholder_person.jpg" class="pics">
+                            <div class="textoverlay"><p class="names">Sam Rowe</p></div>
+                        </a>
+                    </li>
                     <li>
                         <a href="profiles/member_6416585.php">
                             <img src = "images/Placeholder_person.jpg" class="pics">
                             <div class="textoverlay"><p class="names">Robert Bacon</p></div>
                         </a>
                     </li>
-                    <li>Thushanthy Thillainathan <img src = "images/Placeholder_person.jpg" class="pics"></li>
-                    <li>Robbie Murray <a href="profiles/member_6437280.php" target="_blank" ><img src = "images/Placeholder_person.jpg" class="pics" ></a></li>
-                    <li>Lukas Rygh <img src = "images/Placeholder_person.jpg" class="pics"></li>
+                    <li> 
+                        <a href="profiles/member_6460017.php">
+                            <img src = "images/Placeholder_person.jpg" class="pics">
+                            <div class="textoverlay"><p class="names">Thushanthy Thillainathan</p></div>
+                        </a>
+                    </li>
+                    <li> 
+                        <a href="profiles/member_6437280.php" target="_blank" >
+                            <img src = "images/Placeholder_person.jpg" class="pics">
+                            <div class="textoverlay"><p class="names">Robbie Murray</p></div>
+                        </a>
+                    </li>
+                    <li> 
+                        <a href="profiles/member_6429748.php.php">
+                            <img src = "images/Placeholder_person.jpg" class="pics">
+                            <div class="textoverlay"><p class="names">Lukas Rygh</p></div>
+                        </a>
+                    </li>
                 </ul>    
     </body>
 </html>
