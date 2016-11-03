ca200116f90f796d713af6e5c226ddabf5072179
Sam Rowe <samrowe98@gmail.com>
2016-11-03 14:39:04 +0000

    Updated Sam page, added table to index, moved links css to index style sheet and added title attribute to index

--- index.php
+++ index.php
@@ -6,8 +6,17 @@
         <title>Group 15</title>
         <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
 		<style type = "text/css">
+		ul{
+			margin: auto;
+			text-align: center;
+			padding: 100px
+			}
 
-
+		li {
+			display: inline-block;
+			width: 150px;
+			position: relative;
+		}
 		</style>
 	<link href='https://fonts.googleapis.com/css?family=Roboto:300,400,700' rel='stylesheet' type='text/css'>
         
@@ -15,7 +24,8 @@
     </head>
     <body>
         <h1 style="text-align:center">Group 15's Page!</h1>
-		<p>This is group 15's main page! The members of our group are:</p>
+		<p>This is group 15's main page! To access any of our members individual pages please click on their image below. This site will contain information about our members and their hobbie and interests. Their work email is also listed on this page should you wish to contact them. Enjoy!</p>
+		<h3 title="Click the image of the member to access their page">Our Members:</h3>
 		<ul>
                     <li>
                         <a href="profiles/member_6426813.php" target=_blank>
@@ -53,6 +63,39 @@
                             <div class="textoverlay"><p class="names">Lukas Rygh</p></div>
                         </a>
                     </li>
-                </ul>    
+                </ul>
+				<p>Below is the table containing all of our groups work emails if you need to contact them for any reason.</p>
+			<table border="1" cellspacing="3" cellpadding="3">
+				<caption>Members of Group 15 Contact Emails</caption>
+				<tr>
+					<th>Name</th>
+					<th>Email</th>
+				</tr>
+				<tr>
+					<td>Sam Rowe</td>
+					<td>sr00584@surrey.ac.uk</td>
+				</tr>
+				<tr>
+					<td>Robbie Murray</td>
+					<td>rm00727@surrey.ac.uk</td>
+				</tr>
+				<tr>
+					<td>Lukas Rygh</td>
+					<td>lr00341@surrey.ac.uk</td>
+				</tr>
+				<tr>
+					<td>Callum Seymour</td>
+					<td>cs00916@surrey.ac.uk</td>
+				</tr>
+				<tr>
+					<td>Robert Bacon</td>
+					<td>rb00573@surrey.ac.uk</td>
+				</tr>
+				<tr>
+					<td>Thushanthy Thillainathan</td>
+					<td>tt00308@surrey.ac.uk</td>
+				</tr>
+			</table>
+				
     </body>
 </html>
