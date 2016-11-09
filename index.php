68f9843bed1432615f550fe5b4f587ca37281de4
Thusha27 <thushanthythillainathan@gmail.com>
2016-11-09 11:49:39 +0000

    Add files via upload
    I have made changes in welcome text and also I did some js for submit comment button. click on that button and try

--- index.php
+++ index.php
@@ -5,40 +5,41 @@
     <head>
         <title>Group 15</title>
         <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
-	<link type="text/css" rel="stylesheet" href="css/group.css">
-	<style type = "text/css">
-            ul{
-                    margin: auto;
-                    text-align: center;
-                    padding: 100px
-                    }
+		 <link type="text/css" rel="stylesheet" href="css/group.css">
+		<style type = "text/css">
+		ul{
+			margin: auto;
+			text-align: center;
+			padding: 100px
+			}
 
-            li {
-                    display: inline-block;
-                    width: 150px;
-                    position: relative;
-            }
-            table {
-                border-collapse:collapse;
-            }
-            th {
-                background-color: #91EFA6;
-            }
-        </style>
+		li {
+			display: inline-block;
+			width: 150px;
+			position: relative;
+		}
+                table {
+                    border-collapse:collapse;
+                }
+                th {
+                    background-color: #91EFA6;
+                }
+#split container{
+height:50%;
+}
 
-        <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,700' rel='stylesheet' type='text/css'>
+		</style>
+	<link href='https://fonts.googleapis.com/css?family=Roboto:300,400,700' rel='stylesheet' type='text/css'>
         
-        <script>
-            $(function() {
-                $("#mailtoform").submit(function(e) {
-                    $("#contactForm").hide();
-                    $("#contactFormSubmitted").show();
-                });
-            });
-        </script>
+        
     </head>
-    <body>      
+    <body>
+        <div id="split container">
+    <main>
+<section class ="half">
+
         <h1 style="text-align:center">Group 15's Page!</h1>
+        <h2><marquee>Welcome To Our Group Page</marquee></h2>
         <p>This is group 15's main page! To access any of our members individual pages please click on their image below. This site will contain information about our members and their hobbie and interests. Their work email is also listed on this page should you wish to contact them. Enjoy!</p>
         <h3 title="Click the image of the member to access their page">Our Members:</h3>
         <ul>
@@ -79,9 +80,12 @@
                 </a>
             </li>
         </ul>
-        <div style="overflow:auto;">
+
+</section>
+<section class ="half2">
+        <div>
             <p>Below is the table containing all of our groups work emails if you need to contact them for any reason.</p>
-            <table style="float:left;" border="1" cellspacing="3" cellpadding="3">
+            <table border="1" cellspacing="3" cellpadding="3">
                     <caption>Members of Group 15 Contact Emails</caption>
                     <tr>
                             <th id="white">Name</th>
@@ -112,21 +116,25 @@
                             <td>tt00308@surrey.ac.uk</td>
                     </tr>
             </table>
-            <div style="position:relative;" id="contactForm" class="contactForm">
+            <div class="contactform">
                 <form action="mailto:sr00584@surrey.ac.uk,rm00727@surrey.ac.uk,lr00341@surrey.ac.uk,cs00916@surrey.ac.uk,rb00573@surrey.ac.uk,tt00308@surrey.ac.uk" id="mailtoform" method="POST">
                     <label>Your Email:</label><br />
                     <input type="email"><br />
                     <label>Subject:</label><br />
                     <input type="text"><br />
                     <label>Comment:</label><br />
-                    <textarea form="mailtoform"></textarea><br /> <br />                     
-                    <input type="submit" value="Submit Comment">
+                    <textarea form="mailtoform"></textarea><br /> <br />   
+                 
+                    <button onClick ="myFunction()">Submit Comment</button>
+<script>
+function myFunction(){
+alert("Thank You");
+}
+</script>
                 </form>
             </div>	
-            <div style="display:none" id="contactFormSubmitted" class="contactForm">
-                <p>Form Submitted!</p>
-            </div>
         </div>
+</section>
+</div>
     </body>
-    <?php include('./common/footer.php') ?>
 </html>
