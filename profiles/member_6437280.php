636d5e628481662d0848dbb075f57732f35928dc
RobbieMurray <robbodym@gmail.com>
2016-11-10 13:32:10 +0000

    Added data to group xml, made my own xml

--- profiles/member_6437280.php
+++ profiles/member_6437280.php
@@ -32,6 +32,15 @@
                 color:#A0A0A0;
             }
             
+            html {
+                background-image: url("../images/BackgroundDraft2.png");
+            }
+            
+            div {
+                background-color: "E0E0E0";
+                opacity: 1;
+            }
+            
         </style>
     </head>
     <body>
@@ -43,39 +52,42 @@
         
         <img src = "../images/Placeholder_person.jpg" class='pics'>
         
-        <p>
-            Here is a list of things I like.
-        </p>
+        <div>
+            <p>
+                Here is a list of things I like.
+            </p>
+
+            <?php
+            $interests = array("Playing Drums", "Skiing", "Programming", "Taking Photos",
+            "Cooking", "Running", "Playing Counter Strike", "Sleeping", "Eating", "Rock Climbing"); ?>
+
+            <ul>
+                <?php 
+                shuffle($interests);
+                foreach ($interests as $interests): ?> 
+                <li> <?php echo $interests; ?></li>
+                <?php endforeach;
+                unset($interests);?>
+            </ul>
+
+            <p> 
+                Here are my three extreme sports that I want to try out
+            </p>
+
+            <?php
+            $sports = array("Paragliding", "Skydiving", "Sucba Diving", "Kite Surfing",
+            "Mountain Biking", "Bungee Jumping","White Water Rafting", "Walking on Water");
+            $rand_keys = array_rand($sports, 3);
+            echo $sports[$rand_keys[0]] . ", " . "\n";
+            echo $sports[$rand_keys[1]] . ", " . "\n";
+            echo $sports[$rand_keys[2]] . "\n";
+            ?>
+
+            <p>
+                <br>
+            </p>
+        </div>
         
-        <?php
-        $interests = array("Playing Drums", "Skiing", "Programming", "Taking Photos",
-        "Cooking", "Running", "Playing Counter Strike", "Sleeping", "Eating", "Rock Climbing"); ?>
-        
-        <ul>
-            <?php 
-            shuffle($interests);
-            foreach ($interests as $interests): ?> 
-            <li> <?php echo $interests; ?></li>
-            <?php endforeach;
-            unset($interests);?>
-        </ul>
-         
-        <p> 
-            Here are my three extreme sports that I want to try out
-        </p>
-        
-        <?php
-        $sports = array("Paragliding", "Skydiving", "Sucba Diving", "Kite Surfing",
-        "Mountain Biking", "Bungee Jumping","White Water Rafting", "Walking on Water");
-        $rand_keys = array_rand($sports, 3);
-        echo $sports[$rand_keys[0]] . ", " . "\n";
-        echo $sports[$rand_keys[1]] . ", " . "\n";
-        echo $sports[$rand_keys[2]] . "\n";
-        ?>
- 
-        <p>
-        <br>
-        </p>
         
         <table style="border-collapse: collapse;">
             <tr>
