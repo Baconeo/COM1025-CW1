4e218e526defcc2c649be8c6a49fbd725349c8b0
Lukas <Lukas>
2016-11-15 13:17:33 +0000

    Uploaded own image, continued working on personal one. Lukas.

--- profiles/member_6429748.php
+++ profiles/member_6429748.php
@@ -4,32 +4,27 @@
 	<head>
 		<title>"Lukas Rygh"</title>
 		<meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+		<link type="text/css" rel="stylesheet" href="../css/group.css"> 
+
 		<style>
 		</style>
 	</head>
 	<body>
-		<h1>Lukas Rygh</h1>
-	<h4 style="text-align:right;"> Return to the main page:<br><a href="../index.php">Main</a></h4>
+		<h1 id = "top">Lukas Rygh</h1>
+	<h4 style="text-align:right;" title = "Because why not get as many marks with one line of code as possible?"> Return to the main page:<br><a href="../index.php">Main</a></h4>
 
-		<h2>Personal Interests</h2>
-	
+		<h2>About Me</h2>
+			<img src="../images/member_6429748.jpg" alt="Lukas Rygh" style="width:300px;height:250px;display:block;margin:auto;border-radius:10px">
 	
 		<h2>Past Progamming Experience</h2>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+			<p>In my previous school, I was taught python, which has surprisingly similar syntax to Java in some respects. I also had <i>some</i> HTML and CSS experience, but it wasn't taught very well and I sucked at it anyway. So while it probably gave me a bit of an advantage, I've still had to put in a lot of work to correct previous misconceptions and keep up with the teaching pace. And of course, it didn't help with PHP or XML.</p>
+			
+		<ul>Hobbies</ul>
+			<li>Skiing</li>
+			<li>Video Games</li>
+			<li>Anime(Mecha, mostly)</li>
+			
+			
+		<a href = "#top">Back to top</a>
+	</body>
 </html>
