6addd3fd4c46f34697a6c21bdebc23c45e9c7889
bacon_ <mrrobacon@gmail.com>
2016-11-18 11:00:20 +0000

    Index fixed (no errors)

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <!DOCTYPE HTML>
 <html lang="en">
-    <?php include('./common/header.php') ?>
-
     <head>
         <title>Group 15</title>
-        <meta content="charset=UTF-8">
+        <meta http-equiv="Content-type" content="text/html;charset=utf-8" />
             <link type="text/css" rel="stylesheet" href="css/group.css">
             <script type="text/javascript" src="../jQuery/jquery-3.1.1.min.js"></script>
             <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,700' rel='stylesheet' type='text/css'> 
@@ -23,6 +21,11 @@
 		}
                 table {
                     border-collapse:collapse;
+                }
+                table, th, td {
+                    border: 1px solid gray;
+                    padding: 3px;
+                    spacing: 3px;
                 }
                 th {
                     background-color: #91EFA6;
@@ -48,52 +51,52 @@
         </script>
     </head>
     <body>
-        <div id="split container">
+        <?php include('./common/header.php') ?>        
+        <div id="split-container">
             <section id="half" class ="half">
-            <header>
-                <h1 style="text-align:center">Group 15's Page!</h1>
-                <h2><marquee>Welcome To Our Group Page</marquee></h2>
-				<h3>Page Navigation:</h3>
-				<h5><a href="#table" class="topLink">-Member Information Table</a></h5>
-				<h3>Introduction</h3>
-                <p>This is group 15's main page! To access any of our members individual pages please click on their image below. This site will contain information about our members and their hobbie and interests. Their work email is also listed on this page should you wish to contact them. Enjoy!</p>
-            </header>
-            <main>
+                <header>
+                    <h1 style="text-align:center">Group 15's Page!</h1>
+                    <h2>Welcome To Our Group Page</h2>
+                                    <h3>Page Navigation:</h3>
+                                    <h5><a href="#table" class="topLink">-Member Information Table</a></h5>
+                                    <h3>Introduction</h3>
+                    <p>This is group 15's main page! To access any of our members individual pages please click on their image below. This site will contain information about our members and their hobbie and interests. Their work email is also listed on this page should you wish to contact them. Enjoy!</p>
+                </header>
                 <h3 title="Click the image of the member to access their page">Our Members:</h3>
                 <ul>
                     <li>
                         <a href="profiles/member_6426813.php">
-                            <img src = "images/member_6426813.jpg" class="pics">
+                            <img alt="An image of Callum" src = "images/member_6426813.jpg" class="pics">
                             <div class="textoverlay"><p class="names" title="Callum Seymour" >Callum Seymour</p></div>
                         </a>
                     </li>
                     <li> 
                         <a href="profiles/member_6407279.php">
-                            <img src = "images/member_6407279.jpg" class="pics">
+                            <img alt="An image of Sam" src = "images/member_6407279.jpg" class="pics">
                             <div class="textoverlay"><p class="names" title="Sam Rowe" >Sam Rowe</p></div>
                         </a>
                     </li>
                     <li>
                         <a href="profiles/member_6416585.php">
-                            <img src = "images/member_6416585.jpg" class="pics">
+                            <img alt="An image of Robert" src = "images/member_6416585.jpg" class="pics">
                             <div class="textoverlay"><p class="names" title="Robert Bacon" >Robert Bacon</p></div>
                         </a>
                     </li>
                     <li> 
                         <a href="profiles/member_6460017.php">
-                            <img src = "images/member_6460017.jpg" class="pics">
+                            <img alt="An image of Thushanthy" src = "images/member_6460017.jpg" class="pics">
                             <div class="textoverlay"><p class="names" title="Thushanthy Thillainathan" >Thushanthy Thillainathan</p></div>
                         </a>
                     </li>
                     <li> 
                         <a href="profiles/member_6437280.php">
-                            <img src = "images/Placeholder_person.jpg" class="pics">
+                            <img alt="An image of Robbie" src = "images/Placeholder_person.jpg" class="pics">
                             <div class="textoverlay"><p class="names" title="Robie Murray" >Robbie Murray</p></div>
                         </a>
                     </li>
                     <li> 
                         <a href="profiles/member_6429748.php">
-                            <img src = "images/Placeholder_person.jpg" class="pics">
+                            <img alt="An image of Lukas" src = "images/Placeholder_person.jpg" class="pics">
                             <div class="textoverlay"><p class="names" title="Lukas Rygh" >Lukas Rygh</p></div>
                         </a>
                     </li>
@@ -102,61 +105,59 @@
             <section id="half2" class ="half2">
                 <div style="overflow:auto;">
                     <?php
-                            $subtitle = "Contact Information";
-                            echo "<h3>$subtitle</h3>";
+                        $subtitle = "Contact Information";
+                        echo "<h3>$subtitle</h3>";
                     ?>
                     <p>Below is the table containing all of our groups work emails if you need to contact them for any reason.</p>
-                    <table style="float:left;" border="1" cellspacing="3" cellpadding="3" id="table">
+                    <table style="float:left;" id="table">
 			<!--Linked to the group XML file and reading out the infromation into a table-->
                         <?php
                         $xml=simplexml_load_file("data/group.xml"); 
                         ?>
-                            <caption>Members of Group 15 Details</caption>
-                            <tr>
-                                    <th id="white">Name</th>
-                                    <th id="white">Email</th>
-                                    <th id="white">URN</th>
-                                    <th id="white">Course</th>
-                            </tr>
-                            <tr>
-                                    <td><?php echo $xml->student[0]->name; ?></td>
-                                    <td><?php echo $xml->student[0]->email; ?></td>
-                                    <td><?php echo $xml->student[0]->URN; ?></td>
-                                    <td><?php echo $xml->student[0]->coursetitle; ?></td>
-                            </tr>
-                            <tr>
-                                    <td><?php echo $xml->student[1]->name; ?></td>
-                                    <td><?php echo $xml->student[1]->email; ?></td>
-                                    <td><?php echo $xml->student[1]->URN; ?></td>
-                                    <td><?php echo $xml->student[1]->coursetitle; ?></td>
-                            </tr>
-                            <tr>
-                                    <td><?php echo $xml->student[2]->name; ?></td>
-                                    <td><?php echo $xml->student[2]->email; ?></td>
-                                    <td><?php echo $xml->student[2]->URN; ?></td>
-                                    <td><?php echo $xml->student[2]->coursetitle; ?></td>
-                            </tr>
-                            <tr>
-                                    <td><?php echo $xml->student[3]->name; ?></td>
-                                    <td><?php echo $xml->student[3]->email; ?></td>
-                                    <td><?php echo $xml->student[3]->URN; ?></td>
-                                    <td><?php echo $xml->student[3]->coursetitle; ?></td>
-                            </tr>
-                            <tr>
-                                    <td><?php echo $xml->student[4]->name; ?></td>
-                                    <td><?php echo $xml->student[4]->email; ?></td>
-                                    <td><?php echo $xml->student[4]->URN; ?></td>
-                                    <td><?php echo $xml->student[4]->coursetitle; ?></td>
-                            </tr>
-                            <tr>
-                                    <td><?php echo $xml->student[5]->name; ?></td>
-                                    <td><?php echo $xml->student[5]->email; ?></td>
-                                    <td><?php echo $xml->student[5]->URN; ?></td>
-                                    <td><?php echo $xml->student[5]->coursetitle; ?></td>
-                            </tr>
+                        <caption>Members of Group 15 Details</caption>
+                        <tr>
+                                <th class="white">Name</th>
+                                <th class="white">Email</th>
+                                <th class="white">URN</th>
+                                <th class="white">Course</th>
+                        </tr>
+                        <tr>
+                                <td><?php echo $xml->student[0]->name; ?></td>
+                                <td><?php echo $xml->student[0]->email; ?></td>
+                                <td><?php echo $xml->student[0]->URN; ?></td>
+                                <td><?php echo $xml->student[0]->coursetitle; ?></td>
+                        </tr>
+                        <tr>
+                                <td><?php echo $xml->student[1]->name; ?></td>
+                                <td><?php echo $xml->student[1]->email; ?></td>
+                                <td><?php echo $xml->student[1]->URN; ?></td>
+                                <td><?php echo $xml->student[1]->coursetitle; ?></td>
+                        </tr>
+                        <tr>
+                                <td><?php echo $xml->student[2]->name; ?></td>
+                                <td><?php echo $xml->student[2]->email; ?></td>
+                                <td><?php echo $xml->student[2]->URN; ?></td>
+                                <td><?php echo $xml->student[2]->coursetitle; ?></td>
+                        </tr>
+                        <tr>
+                                <td><?php echo $xml->student[3]->name; ?></td>
+                                <td><?php echo $xml->student[3]->email; ?></td>
+                                <td><?php echo $xml->student[3]->URN; ?></td>
+                                <td><?php echo $xml->student[3]->coursetitle; ?></td>
+                        </tr>
+                        <tr>
+                                <td><?php echo $xml->student[4]->name; ?></td>
+                                <td><?php echo $xml->student[4]->email; ?></td>
+                                <td><?php echo $xml->student[4]->URN; ?></td>
+                                <td><?php echo $xml->student[4]->coursetitle; ?></td>
+                        </tr>
+                        <tr>
+                                <td><?php echo $xml->student[5]->name; ?></td>
+                                <td><?php echo $xml->student[5]->email; ?></td>
+                                <td><?php echo $xml->student[5]->URN; ?></td>
+                                <td><?php echo $xml->student[5]->coursetitle; ?></td>
+                        </tr>
                     </table>
-            </main>
-            <footer>
                     <div style="position:relative;" id="contactForm" class="contactForm">
                         <form action="mailto:sr00584@surrey.ac.uk,rm00727@surrey.ac.uk,lr00341@surrey.ac.uk,cs00916@surrey.ac.uk,rb00573@surrey.ac.uk,tt00308@surrey.ac.uk" id="mailtoform" method="POST">
                             <label>Your Email:</label><br />
@@ -172,7 +173,6 @@
                         <p>Form Submitted!</p>
                     </div>
                 </div>
-            </footer>
             </section>
         </div>
         <?php include('./common/footer.php') ?>
